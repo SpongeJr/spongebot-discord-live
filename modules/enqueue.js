1d00758c4699d567667e8152849310739fa8069d
Josh <spongejr@gmail.com>
2018-03-12 10:59:10 -0400

    fleshing out enqueue stuff
    Still in the queue for enqueue:
    [ 1 ] Sponge ··· To-Do List
    [ 2 ] _ ··· Insert into
    [ 3 ] _ ··· Justification/output formatting/prettying up
    [ 4 ] _ ··· Resolve @mentions back to nick names and use those when used
    [ 5 ] _ ··· !q - with no add'l arguments should remove #1 entry
    [ 6 ] _ ··· Add !q next command to remove the #1 entry plus also congratulate performer and introduce next one
    [ 7 ] _ ··· `!q - <name> [all]` command to remove the next(?) song by <name> or all songs in the list by them
    [ 8 ] _ ··· After every "!q next", save data from who just finished to a "completed performances" log file

--- modules/enqueue.js
+++ modules/enqueue.js
@@ -1,8 +1,16 @@
 const cons = require('../lib/constants.js');
 const utils = require('../lib/utils.js');
-const chSend = utils.chSend;
+var queue = require('../' + cons.DATA_DIR + cons.ENQUEUE_FILE);
 
-var Command = utils.Command;
+
+const Command = utils.Command;
+const Song = function(config) {
+	
+	if (!config) {config = {};}
+	
+	this.performer = config.performer || 'An unnamed performer',
+	this.song = config.song || 'An unnamed song'
+};
 
 module.exports = {
 	commands: {
@@ -13,7 +21,74 @@ module.exports = {
 			accessRestrictions: false,
 			cmdGroup: 'Fun and Games',
 			do: function(message, parms, gameStats) {
-				chSend(message, 'WIP :rip:');
+				parms = parms.split(' ');		
+				if (parms[0] === '') {
+					
+					if (queue.length === 0) {
+						utils.chSend(message, 'The queue is empty.');
+						return;
+					}
+					
+					utils.chSend(message, 'Showing queue...');
+					
+					let str = '```';
+					for (let i = 0; i < queue.length; i++) {
+						str += '[ ' + (i + 1) + ' ] ';
+						str += queue[i].performer + " ··· " + queue[i].song;
+						str += '\n';
+						
+					}
+					str += '```';
+					utils.chSend(message, str);
+
+					return;
+				}
+				parms[0] = parms[0].toLowerCase();
+				let subCmd = parms[0];
+				
+				if (this.subCmd.hasOwnProperty(parms[0])) {
+					//we've found a found sub-command, so do it...
+					parms.shift();
+					parms = parms.join(' ');
+					this.subCmd[subCmd].do(message, parms, gameStats);
+				} else {
+					utils.chSend(message, 'Please see `!help q` for help with the queue command.');
+				}
+			},
+			subCmd: {
+				"+": {
+					do: function(message, parms, gameStats) {
+						
+						parms = parms.split(' ');
+						let who = parms[0];
+						parms.shift();
+						let song = parms.join(' ');
+						
+						queue.push(new Song({"performer": who, "song": song}));
+						utils.saveObj(queue, cons.ENQUEUE_FILE);
+						utils.chSend(message, 'Added a song to queue. .length is now: ' + queue.length);
+					}
+				},
+				"-": {
+					do: function(message, parms, gameStats) {
+						
+						if (queue.length === 0) {
+							utils.chSend(message, 'The queue is already empty!');
+							return;
+						}
+						
+						let num = parseInt(parms);
+						
+						if ((num > queue.length) || (num < 1) || (isNaN(num))) {
+							utils.chSend(message, 'Can\'t remove that number. There are ' + queue.length + ' in the queue.');
+							return;
+						}
+						
+						let removed = queue.splice(num - 1, 1);
+						utils.chSend(message, 'OK, removing song #' + num + ' (' + removed[0].song +
+						  ' by ' + removed[0].performer + ')');
+					}
+				}
 			}
 		}
 	}
