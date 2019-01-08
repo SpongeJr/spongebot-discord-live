2940a2339554a399241ef13c9344539d781edc18
Josh <spongejr@gmail.com>
2019-01-08 05:49:58 -0500

    !scram improvements!
    NEW: !scram configuration sets
    NEW: !scram loadConfig command to switch configs
    NEW: "Lightning mode" config (1 second between words)
    FIXED/IMPROVED: Timer to new word now based upon when word was guessed (or expired) rather than when word was presented
    NEW: !scram top subcommand to list top ten winningest !scramblers
    
    TO DO: Improve !scram top, add !scram fastest
    TO DO: config switching only done by list of allowed roles (or everyone, if desired), per server configurable (maybe)

--- games/scram.js
+++ games/scram.js
@@ -6,29 +6,28 @@ var scramWordLists = {
 	"402126095056633859": cons.PLANET_SCRAMWORDS
 };
 
-/*
-var scramConfig = {
-	wordDelay: 105000,
-	wordDelayVariation: 15000,
-	baseAward: 600,
-	letterBounus: 100, 
-	guessTime: 29000,
-	extraGuessTime: 2500,
-	maxMultiplier: 2,
-	minMultiplier: 1
+var scramConfigs = {
+	"default": {
+		wordDelay: 105000,
+		wordDelayVariation: 15000,
+		baseAward: 600,
+		letterBounus: 100, 
+		guessTime: 29000,
+		extraGuessTime: 2500,
+		maxMultiplier: 2,
+		minMultiplier: 1
+	},
+	"lightning": {
+		wordDelay: 1000,
+		wordDelayVariation: 0,
+		baseAward: 0,
+		letterBounus: 0, 
+		guessTime: 29000,
+		extraGuessTime: 2500,
+		maxMultiplier: 2,
+		minMultiplier: 1
+	}
 };
-*/
-var scramConfig = {
-	wordDelay: 1000,
-	wordDelayVariation: 0,
-	baseAward: 0,
-	letterBounus: 0, 
-	guessTime: 29000,
-	extraGuessTime: 2500,
-	maxMultiplier: 2,
-	minMultiplier: 1
-};
-
 //-----------------------------------------------------------------------------
 var scrambler = function(inputWord) {
 	var wordArray = inputWord.split("");
@@ -43,13 +42,80 @@ var scrambler = function(inputWord) {
 };
 //-----------------------------------------------------------------------------
 module.exports = {
-	subCmd: {},
+	subCmd: {
+		loadconfig: {
+			do: function(message, parms) {
+				var server = message.guild;
+				var outStr = '';
+				var configs = Object.keys(scramConfigs);
+				if (configs.includes(parms[1])) {
+					scram[server.id].currentConfig = parms[1];
+					outStr += ' Config successfully changed to: ' + parms[1];
+				} else {
+					outStr += ` I see no scram config called ${parms[1]} here!`;
+				}
+				utils.chSend(message, outStr);
+			}
+		},
+		saveconfig: {
+			do: function(message, parms) {
+				var server = message.guild;
+				var outStr = 'You find yourself unable to do that at this time. Strange.';
+				utils.chSend(message, outStr);
+			}
+		},
+		top: {
+			do: function(message, parms, gameStats) {
+				let scramStats = {};
+				let outStr = '';
+				let topWins = [];
+				let fastest = [];
+				
+				// build scramStats from gameStats
+				for (let who in gameStats) {
+					let game = 'scram';
+					for (let stat in gameStats[who][game]) {	
+						if (!scramStats.hasOwnProperty(stat)) {
+							scramStats[stat] = {};
+						}
+						scramStats[stat][who] = gameStats[who][game][stat];
+					}
+				}
+				
+				// build arrays and sort
+				for (let who in scramStats.wins) {
+					topWins.push( {"who": who, "wins": scramStats.wins[who]} );
+					fastest.push( {"who": who, "speed": scramStats.fastest[who]} );
+				}
+				
+				topWins.sort(utils.objSort("wins", -1));
+				fastest.sort(utils.objSort("speed"));
+
+				// output top 10
+				let topNick = utils.idToNick(topWins[0].who, gameStats);
+				outStr += '```TOP 10 DESCRAMBLERS:\n';
+				outStr += ` !SCRAM MASTER :  -> ${topNick} <-  with ${topWins[0].wins} wins \n\n`;
+				for (let num = 1; (num < 10 && num < topWins.length); num++) {
+					let nick = utils.idToNick(topWins[num].who, gameStats);
+					outStr += ` #${num + 1}: (${topWins[num].wins}) ... ${nick} \n`;
+				}
+				
+				outStr += '```';
+				
+				utils.chSend(message, outStr);
+				
+				// scramStats.wins
+				
+				
+			}
+		}
+	},
 	s: {
 		do: function(message, parms, gameStats, bankroll) {
 			var server = message.guild;
 			var theWord = scram[server.id].word;
-			var minMultiplier = scramConfig.minMultiplier;
-			var maxMultiplier = scramConfig.maxMultiplier;
+			var minMultiplier = scramConfigs[scram[server.id].currentConfig].minMultiplier;
+			var maxMultiplier = scramConfigs[scram[server.id].currentConfig].maxMultiplier;
 			if (!server) {
 				utils.auSend(message, 'The word scramble game is meant to be played in public, and '+
 				'not direct messages. Sorry! It\'s more fun with others, anyway!');
@@ -87,10 +153,10 @@ module.exports = {
 				var speed = now - scram[server.id].guessStartTime;
 				var fastest = utils.getStat(who, 'scram', 'fastest', gameStats) || 0;
 				outStr += (speed / 1000).toFixed(1) + ' seconds and wins ';
-				var guessTime = scramConfig.guessTime + scramConfig.extraGuessTime * theWord.length; // max allowed time
+				var guessTime = scramConfigs[scram[server.id].currentConfig].guessTime + scramConfigs[scram[server.id].currentConfig].extraGuessTime * theWord.length; // max allowed time
 				var multiplier = 1 - speed / guessTime;
-				multiplier = (multiplier * (scramConfig.maxMultiplier - scramConfig.minMultiplier) + minMultiplier);
-				var baseAward = scramConfig.baseAward + scramConfig.letterBounus * theWord.length;
+				multiplier = (multiplier * (scramConfigs[scram[server.id].currentConfig].maxMultiplier - scramConfigs[scram[server.id].currentConfig].minMultiplier) + minMultiplier);
+				var baseAward = scramConfigs[scram[server.id].currentConfig].baseAward + scramConfigs[scram[server.id].currentConfig].letterBounus * theWord.length;
 				var award = parseInt(baseAward * multiplier);
 
 				outStr +=  `${award} credits! ( ${baseAward} x ~${multiplier.toFixed(2)} speed multiplier )`;
@@ -110,8 +176,8 @@ module.exports = {
 				  gameStats[message.author.id].scram.wins + ' words!';
 				}
 				
-				var theDelay = parseInt(Math.max(1, scramConfig.wordDelay - (scramConfig.wordDelayVariation / 2) +
-				  Math.random() * scramConfig.wordDelayVariation));
+				var theDelay = parseInt(Math.max(1, scramConfigs[scram[server.id].currentConfig].wordDelay - (scramConfigs[scram[server.id].currentConfig].wordDelayVariation / 2) +
+				  Math.random() * scramConfigs[scram[server.id].currentConfig].wordDelayVariation));
 				outStr += '\n Next word available in ' + parseInt(theDelay / 1000) + ' second(s).';			
 				scram[server.id].timer = setTimeout(function() {
 					if (scram[server.id].runState !== 'ready') {
@@ -135,18 +201,6 @@ module.exports = {
 			'not direct messages. Sorry! It\'s more fun with others, anyway!');
 			return;
 		}
-		
-		parms = parms.split(' ');
-		if (parms[0] !== '') {
-			parms[0] = parms[0].toLowerCase();
-			if (this.subCmd.hasOwnProperty(parms[0])) {
-				//we've found a found sub-command, so do it...
-				this.subCmd[parms[0]].do(message, parms);
-				return; // we're done here
-			}
-			// ignore non-sub-command extra stuff they type
-		}
-		
 		if (!scram.hasOwnProperty(server.id)) {
 			// key doesn't exist for this server, so init
 			utils.debugPrint('!scram: Adding instance for ' + server.id + ' (' +
@@ -154,6 +208,18 @@ module.exports = {
 			scram[server.id] = {};
 			scram[server.id].announce = true;
 			scram[server.id].runState = 'ready';
+			scram[server.id].currentConfig = 'default';
+		}
+		
+		parms = parms.split(' ');
+		if (parms[0] !== '') {
+			parms[0] = parms[0].toLowerCase();
+			if (this.subCmd.hasOwnProperty(parms[0])) {
+				//we've found a found sub-command, so do it...
+				this.subCmd[parms[0]].do(message, parms, gameStats);
+				return; // we're done here
+			}
+			// ignore non-sub-command extra stuff they type
 		}
 		
 		if (scram[server.id].runState === 'ready') {
@@ -187,20 +253,20 @@ module.exports = {
 			  '   *Category*: ' + theCat);
 			  
 
-			var guessTime = scramConfig.guessTime + scramConfig.extraGuessTime * theWord.length;
+			var guessTime = scramConfigs[scram[server.id].currentConfig].guessTime + scramConfigs[scram[server.id].currentConfig].extraGuessTime * theWord.length;
 			var theMess = 'You have ' + parseInt(guessTime / 1000) + 
 			  ' seconds to guess by typing `!s <guess>`.';
 			utils.chSend(message, theMess);
 			scram[server.id].runState = 'guessing';
 			
 			scram[server.id].guessStartTime = new Date();
-			var theDelay = Math.max(1, parseInt(scramConfig.wordDelay - (scramConfig.wordDelayVariation / 2) +
-			  Math.random() * scramConfig.wordDelayVariation));
+			var theDelay = Math.max(1, parseInt(scramConfigs[scram[server.id].currentConfig].wordDelay - (scramConfigs[scram[server.id].currentConfig].wordDelayVariation / 2) +
+			  Math.random() * scramConfigs[scram[server.id].currentConfig].wordDelayVariation));
 			scram[server.id].guessTimer = setTimeout(function() {
 				if (scram[server.id].runState === 'guessing') {
 					utils.chSend(message, 'The `!scram` word was not guessed' +
-					' in time! The word was: ' + scram[server.id].word) + 
-					'\n Next word available in ' + parseInt(theDelay / 1000) + ' second(s).';
+					  ' in time! The word was: ' + scram[server.id].word + 
+					  '\n Next word available in ' + parseInt(theDelay / 1000) + ' second(s).');
 					
 					scram[server.id].runState = 'gameover';
 					scram[server.id].timer = setTimeout(function() {
@@ -211,7 +277,6 @@ module.exports = {
 							}
 						}	
 					}, theDelay);
-					
 				}
 			}, guessTime);
 		} else {
