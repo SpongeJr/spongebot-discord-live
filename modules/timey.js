c81eeae0ed931a03204407a8761f0c551a3136f9
Josh <spongejr@gmail.com>
2019-12-21 11:21:49 -0500

    events system update
    Can now use !setevent command to setup properties of an event, and then save with !addevent

--- modules/timey.js
+++ modules/timey.js
@@ -1,33 +1,44 @@
 const cons = require('../lib/constants.js');
 const utils = require('../lib/utils.js');
-const events = require('../../data/events.json');
+const events = require('../' + cons.EVENTS_FILENAME);
+const vars = {
+	unsavedEvents: {},
+	requiredProps: ["title", "eventName", "eventTime", "announceTime", "announceText", "announceChannels"]
+};
+const setTimeoutForEvent = function(event, BOT) {
+	// returns: a string indicating result:
+	// "fail", "success", or "announcedLate"
 
-module.exports = {
-	setTimeoutForEvent: function(event, BOT) {
-		// returns: a string indicating result:
-		// "fail", "success", or "announcedLate"
-
-		let now = new Date().valueOf();	
-		let apologyText = cons.TIMEY.lateEventAnnounceText;
-		if (event.announceTime < now) {
-			// has eventTime passed? If not, announce late...
-			if (event.eventTime < now ) {
-				return "fail";
+	let now = new Date().valueOf();	
+	let apologyText = cons.TIMEY.lateEventAnnounceText;
+	if (event.announceTime < now) {
+		// has eventTime passed? If not, announce late...
+		if (event.eventTime < now ) {
+			return "fail";
+		} else {
+			event.announceChannels.forEach((chanId) => {
+				BOT.channels.get(chanId).send(apologyText + event.announceText);
+			});
+			return "announcedLate";
+		}
+	} else {		
+		setTimeout(function() {
+			
+			if (typeof event.announceChannels === "string") {
+				// single channel string
+				BOT.channels.get(event.announceChannels).send(event.announceText);
 			} else {
-				event.announceChannels.forEach((chanId) => {
-					BOT.channels.get(chanId).send(apologyText + event.announceText);
-				});
-				return "announcedLate";
-			}
-		} else {		
-			setTimeout(function() {
+				// assume array of channel strings
 				event.announceChannels.forEach((chanId) => {
 					BOT.channels.get(chanId).send(event.announceText);
 				});
-			}, event.announceTime - now);
-		}
-		return "success";
-	},
+			}
+		}, event.announceTime - now);
+	}
+	return "success";
+};
+module.exports = {
+	setTimeoutForEvent: setTimeoutForEvent,
 	init: function(BOT) {	
 		console.log("-- timey.init(): Setting event timers from event.json...");
 		
@@ -119,14 +130,102 @@ module.exports = {
 		setevent: {
 			subCmd: {},
 			help: "Setup an event.",
-			longHelp: "Setup an event. Syntax: addevent <property> <value>\nUse `addevent` to save."
+			longHelp: "Setup an event. Syntax: setevent <property> <value>\nUse `addevent` to save." +
+			  "\n addevent <property> with no value specified queries a property.",
+			do: function(message, args, gameStats, bankroll, BOT) {
+				let userId = message.author.id;
+				let outP = "";
+				let prop = args[0];
+				args.shift();
+				let value = args.join(" ");
+				let unsaved = vars.unsavedEvents;
+				
+				if (args[0] === "" || typeof args[0] === "undefined") {
+					// no property specified, so list everything	
+					if (typeof unsaved[userId] !== "object") {
+						outP += "Sorry, you don't have an event set up. Use `setevent <property> <value> to start setting one up`!";
+					} else {
+						outP += ` \`EVENT: ${unsaved[userId].title}\``;
+						for (let prop in unsaved[userId]) {
+							outP += `\n${prop}: ${unsaved[userId][prop]}`;
+						}
+					}
+				} else {
+					// property specified...
+					if (value === "" || typeof value === "undefined") {
+						// query
+						if (typeof unsaved[userId][prop] !== "object") {
+							outP += `Your unsaved event does not have a property \`${prop}\`.`;
+							outP += `\nYou can create one with \`setevent ${prop} <value>\``;
+						} else {
+							let queriedVal = unsaved[userId][prop];
+							outP += `Your unsaved event property \`${prop}\`: ${queriedVal}`;
+						}
+					} else {
+						// set
+						if (typeof unsaved[userId] !== "object") {
+							outP += "Created a new unsaved event for you.\n";
+							vars.unsavedEvents[userId] = {};
+						}
+						vars.unsavedEvents[userId][prop] = value;
+						outP += `Set property \`${prop}\` of your unsaved event to: ${value}`;
+					}
+				}
+				utils.chSend(message, outP);
+			}
 		},
 		addevent: {
 			subCmd: {},
 			help: "Add an event.",
 			longHelp: "Save an event that you have setup with `setevent`.",
-			do: function(message, args, gameStats, gbl) {
-				let requiredProps = ["eventName", "eventTime", "announceTime", "announceText", "announceChannels"];
+			do: function(message, args, gameStats, bankroll, BOT) {
+				let requiredProps = vars.requiredProps;
+				let userId = message.author.id;
+				let outP = "";
+				let unsaved = vars.unsavedEvents;
+				
+				if (typeof unsaved[userId] !== "object") {
+					outP += "Sorry, you don't have an event set up to save. Use `setevent` first!";
+				} else {
+					let failed = false;
+					requiredProps.forEach(function(prop) {
+						if (typeof unsaved[userId][prop] === "undefined") {
+							failed = true;
+							outP += ` :no_entry_sign: Missing property: ${prop}\n`;
+						}
+					});
+					
+					if (!failed) {
+						// for now we use .title as the key in events, and all the other properties under that
+						let title = unsaved[userId].title;
+						events[title] = {};
+						
+						for (let prop in unsaved[userId]) {
+							events[title][prop] = unsaved[userId][prop];
+						}
+						
+						console.log('!addevent: Adding the following record to events.json:');
+						console.log(events[title]);
+						
+						utils.saveObj(events, cons.EVENTS_FILENAME);
+						
+						let thisEvent = events[title];
+						let success = setTimeoutForEvent(events[title], BOT);
+						
+						if (success === "fail") {
+							outP += `-- timey.init(): Event announceTime and eventTime for "${thisEvent.eventName}" ` +
+							  `are both in the past! announceTime: ${thisEvent.announceTime}; now: ${Date.now()}`;
+						} else if (success === "announcedLate") {
+							outP += `-- timey.init(): Event announceTime for "${thisEvent.eventName}" ` +
+							  `is in the past! Announcing immediately! announceTime: ${thisEvent.announceTime}; now: ${Date.now()}`;
+						} else {
+							outP += `Event \`${title}\`has been added... hopefully!`;
+						}
+					} else {
+						outP += "\nEvent not saved, there were errors.";
+					}
+				}
+				utils.chSend(message, outP);
 			}
 		}
 	}
